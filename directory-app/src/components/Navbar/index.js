bb2d65d2139d1c283a14c2951f57f124e4255448
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-27 09:40:16 -0500

    pushing up the rest of the changes

--- directory-app/src/components/Navbar/index.js
+++ directory-app/src/components/Navbar/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-light bg-light">
-            <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Employee Directory</span>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Employee Directory</span>
         </div>
 </nav>
     );
