bdbe2811db4dc88f9d0a18f31696912c50741da3
Jim Nguyen <jimnguyen220@gmail.com>
2021-04-28 01:14:51 -0500

    saving changes, commented out code not working

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -39,28 +39,44 @@ class Table extends Component {
         this.getEmployee(this.state.search);
     };
 
+    // compareBy(key) {
+    //     return function (a,b) {
+    //         if (a[key] < b[key]) return -1;
+    //         if (a[key] > b[key]) return 1;
+    //         return 0;
+    //     }
+    // }
+
+    // sortBy(key) {
+    //     let array = [...this.state.result];
+    //     array.sort(this.compareBy(key));
+    //     this.setState({ result: array })
+    // }
+
 
     render() {
 
         return (
             <>
-            <Search 
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            />
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <EmployeeArray props={this.state.result}/>
-                </tbody>
-            </table>
+                <Search
+                    handleFormSubmit={this.handleFormSubmit}
+                    handleInputChange={this.handleInputChange}
+                />
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col"
+                            // onClick={(e)=>this.sortBy(e, 'name')}
+                            >Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <EmployeeArray props={this.state.result}/>
+                    </tbody>
+                </table>
 
             </>
         );
